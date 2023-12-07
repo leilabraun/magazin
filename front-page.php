93e17b669b86a55cd0a2ac90dfb7a9bbda302e93
leilabraun <leila.braun@stud.fhgr.ch>
2023-12-07 17:49:25 +0100

    end

--- front-page.php
+++ front-page.php
@@ -1,17 +1,18 @@
 <!-- Startseite -->
 <?php get_header(); ?>
 
-<article class="intro-article">
+<article id="intro" class="intro-article">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <div class="intro-image">
         <?php 
-          // Titelbild
+
+          //  Titelbild
           $image = get_field('img');
           if( !empty( $image ) ): ?>
               <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-          <?php endif; ?>
-        </div>
-<!-- Titel -->
+        <?php endif; ?>
+      </div>
+        <!-- Titel -->
         <div class="intro-text">
           <h1><?php the_title() ?></h1>
           <p><?php the_content(); ?></p>
@@ -22,18 +23,19 @@
 
 
 <!-- Absatz 1 -->
-<article>
+<article id="magazinausschnitt-1">
+
   <?php echo esc_html( get_field('text1') ); ?>
   
-    <?php 
-      $image2 = get_field('bild1');
-      if( !empty( $image2 ) ): ?>
+  <?php 
+    $image2 = get_field('bild1');
+    if( !empty( $image2 ) ): ?>
 
-      <div class="bild1">
-        <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
-      </div>
-    <?php endif; ?>
-  
+    <div class="bild bild1">
+      <img class="myImg" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
+    </div>
+  <?php endif; ?>
+
 
   <div class="absatz_1">
     <?php the_field('absatz_1'); ?>
@@ -43,17 +45,18 @@
 
 <!-- Absatz 2 -->
 
-<article>
+<article id="magazinausschnitt-2">
+
 <?php 
   $image2 = get_field('bild2');
       if( !empty( $image2 ) ): ?>
 
-      <div class="bild2">
-        <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
+      <div class="bild bild2">
+        <img class="myImg" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
       </div>
     <?php endif; ?>
 
-<div class="absatz_2">
+  <div class="absatz_2">
     <?php the_field('absatz_2'); ?>
   </div>
 
@@ -61,34 +64,34 @@
 
 <!-- Absatz 3 -->
 
-<article>
-<?php 
-  $image2 = get_field('bild3');
+<article id="magazinausschnitt-3">
+  <?php 
+    $image2 = get_field('bild3');
       if( !empty( $image2 ) ): ?>
 
-      <div class="bild3">
-        <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
-      </div>
-    <?php endif; ?>
+    <div class="bild bild3">
+      <img class="myImg" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
+    </div>
+  <?php endif; ?>
 
-<div class="absatz_3">
+  <div class="absatz_3">
     <?php the_field('absatz_3'); ?>
   </div>
 
 </article>
 
-
-<article>
+<!-- Absatz 4 -->
+<article id="magazinausschnitt-4">
  
-  
     <?php 
       $image2 = get_field('bild4');
       if( !empty( $image2 ) ): ?>
 
-      <div class="bild4">
-        <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
-      </div>
-    <?php endif; ?>
+    <div class="bild bild4">  
+      <img class="myImg" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
+    </div>
+
+  <?php endif; ?>
   
 
   <div class="absatz_4">
@@ -98,13 +101,27 @@
 </article>
 
 
-  <!-- Wenn die Seite leer ist: -->
-  <?php endwhile; else : ?>
-    <p>Aktuell gibt es keinen Inhalt auf dieser Seite.</p> 
-  <?php endif; ?>
+<!-- The Modal -->
+<div id="myModal" class="modal">
 
-  <!-- Mobile -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <!-- The Close Button -->
+  <span class="close">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img01">
+
+</div>
+
+
+
+
+<!-- Wenn die Seite leer ist: -->
+<?php endwhile; else : ?>
+  <p>Aktuell gibt es keinen Inhalt auf dieser Seite.</p> 
+<?php endif; ?>
+
+<!-- Mobile -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 
 <?php get_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     const burgerIcon = document.querySelector('.burger-icon');
+
     const menu = document.querySelector('.menu-div');
 
     burgerIcon.addEventListener('click', function() {
@@ -19,7 +20,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+// Pop Up
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get all elements with class "myImg"
+var images = document.getElementsByClassName("myImg");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Attach the click event to all elements with class "myImg"
+for (var i = 0; i < images.length; i++) {
+  images[i].onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
 
 });
